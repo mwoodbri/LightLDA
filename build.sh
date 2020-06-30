7a7c25ff3c493130daeaef6d8acd04a28cd6256a
Mark Woodbridge <1724545+mwoodbri@users.noreply.github.com>
2020-06-30 10:27:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 git clone -b multiverso-initial https://github.com/hhaoyan/Multiverso.git
 
+sed -i 's/-lmpich -lmpl //' Makefile
+sed -i 's/mpic++/mpicxx/' Makefile Multiverso/Makefile
+sed -i 's/-lmpl //' Multiverso/Makefile
+sed -i 's/mpich mpl //' src/multiverso/CMakeLists.txt src/multiverso_server/CMakeLists.txt
+
 cd Multiverso
 cd third_party
 sh install.sh
